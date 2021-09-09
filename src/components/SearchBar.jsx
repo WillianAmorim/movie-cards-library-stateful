a85cd2aa5ab2fbdccfe95e61505243bd7ec32de5
WillianAmorim <willian.amorim1@outlook.com>
2021-09-09 18:05:46 -0300

    resolvendo problemas de lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,42 @@
-// implement SearchBar component here
 import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
     return (
       <div>
         <form data-testid="search-bar-form">
+          <label data-testid="text-input-label">Inclui o texto</label>
+          <input type="text" value={ searchText } onChange={ onSearchTextChange } data-testid="text-input" />
           
-            <label data-testid="text-input-label">Inclui o texto</label>
-            <input type="text" value={ searchText } onChange={ onSearchTextChange } data-testid="text-input" />
+          <label data-testid="checkbox-input-label" >Mostrar somente favoritos</label>
+          <input type="checkbox"
+            checked="bookmarkedOnly"
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input" />
           
-          
-            <label data-testid="checkbox-input-label" >Mostrar somente favoritos</label>
-            <input type="checkbox" checked="bookmarkedOnly" onChange={ onBookmarkedChange } data-testid="checkbox-input" />
-          
-            <label data-testid="select-input-label">Filtrar por gênero</label>
-            <select value={ selectedGenre } onChange={ onSelectedGenreChange } data-testid="select-input">
-              <option value="" data-testid="select-option">Todos</option>
-              <option value="action" data-testid="select-option">Ação</option>
-              <option value="comedy" data-testid="select-option">Comédia</option>
-              <option value="thriller" data-testid="select-option">Suspense</option>
-            </select>
+          <label data-testid="select-input-label">Filtrar por gênero</label>
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </form>
       </div>
-    )
+    );
   }
 }
 
