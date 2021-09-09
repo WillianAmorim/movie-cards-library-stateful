b5f5b0b037235eb5a4305637fa96032dc6ca3f13
WillianAmorim <willian.amorim1@outlook.com>
2021-09-09 15:11:09 -0300

    feito requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,10 @@ class SearchBar extends React.Component {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props;
     return (
       <div>
-        <form data-testid="search-bar-form"></form>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label">Inclui o texto</label>
+          <input type="text" value={ searchText } onChange={ onSearchTextChange } data-testid="text-input" />
+        </form>
       </div>
     )
   }
