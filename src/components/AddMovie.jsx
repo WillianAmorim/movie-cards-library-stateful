789ba5c567f7333efc200bc1eeed5833d009bdf5
WillianAmorim <willian.amorim1@outlook.com>
2021-09-09 15:42:15 -0300

    feito requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,7 +5,9 @@ class AddMovie extends React.Component {
   render() {
     const { onClick } = this.props;
     return (
-      <></>
+      <div>
+        <form data-testid="add-movie-form"></form>
+      </div>
     )
   }
 }
