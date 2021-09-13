faa0b19dd02c61d357f330d09e7f91c17c023c69
WillianAmorim <willian.amorim1@outlook.com>
2021-09-13 00:52:12 -0300

    resolvendo codigo quebrando

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     searchText: '',
-  //     bookmarkedOnly: false,
-  //     selectedGenre: '',
-  //     movies: this.movies,
-  //   };
-  // }
-
   render() {
     const { movies } = this.props;
     return (
@@ -25,7 +16,7 @@ class MovieLibrary extends React.Component {
 }
 
 MovieLibrary.propTypes = {
-  movies: propTypes.arrayOf(
+  movies: PropTypes.arrayOf(
     PropTypes.object,
   ).isRequired,
 };
