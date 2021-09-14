57490edc96721abbb1bca96f209b281ce2570907
WillianAmorim <willian.amorim1@outlook.com>
2021-09-14 09:31:50 -0300

    requisito 8 resolvendo quebra de codigo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import InputTitle from './InputTitle';
 // import PropTypes from 'prop-types';
 // import Button from './Button';
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const {
+      title,
+    } = this.state;
+
     return (
       <form data-testid="add-movie-form">
-        <div />
+        <InputTitle value={ title } onChange={ this.handleChange } />
       </form>
     );
   }
--- src/components/InputTitle.jsx
+++ src/components/InputTitle.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class InputTitle extends React.Component {
   render() {
+    const { value, onChange } = this.props;
     return (
       <label htmlFor="text" data-testid="title-input-label">
         Título
         <input
           type="text"
           name="title"
-          value={ title }
+          value={ value }
           data-testid="title-input"
-          onChange={ this.handleChange }
+          onChange={ onChange }
         />
       </label>
     );
   }
 }
+
+InputTitle.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+}.isRequired;
+
 export default InputTitle;
